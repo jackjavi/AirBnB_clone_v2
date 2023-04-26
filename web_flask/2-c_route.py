2f8989b43269716c8b2f2256a77286fe04bce2c7
jackjavi <jackmtembete@gmail.com>
2023-04-26 17:04:47 +0000

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,33 +3,33 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Display 'HBNB'
-    /c/<text>: Displays 'C' followed by the value of <text>
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
-    return 'HBNB'
+    return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
     """Displays 'C' followed by the value of <text>."""
-    return 'C' + text.replace('_', ' ')
-    
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
