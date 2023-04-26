a8ac8cbf0d46f26064059b977fb8e88ff319a2b4
jackjavi <jackmtembete@gmail.com>
2023-04-26 17:03:12 +0000

    HBNB.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,26 +3,25 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Display 'HBNB'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
-    return 'HBNB'
+    return "HBNB"
 
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
